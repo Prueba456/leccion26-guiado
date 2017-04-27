82a8699b918cb5bff7ed19bd7bc217884f95d353
Betsi Loayza <betsi18_12@hotmail.com>
2017-04-27 17:15:45 -0500

    estructura

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var contrasena = document.getElementById("contras");
+var confirmar = document.getElementById("confir");
+
+if(contrasena.value != confirmar.value){
+    alert("La contraseña no es igual");
+}
