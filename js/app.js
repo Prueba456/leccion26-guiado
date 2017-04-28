37b6e00163f5346c1173311b0a79ccbba11f180d
Betsi Loayza <betsi18_12@hotmail.com>
2017-04-28 11:28:40 -0500

    validacion

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-var contrasena = document.getElementById("contras");
-var confirmar = document.getElementById("confir");
 
-if(contrasena.value != confirmar.value){
-    alert("La contraseña no es igual");
-}
+var contra1 = document.getElementById("contras");
+var contra2 = document.getElementById("confir");
+window.addEventListener("load", function (e){
+  e.preventDefault();
+  contra2.addEventListener("blur", function(){
+  if (contra1.value == contra2.value){
+    alert("Las dos claves son iguales")
+  } else{
+    alert("Las dos claves son distintas")
+  }
+  })
+})
